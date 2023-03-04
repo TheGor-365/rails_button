3da7dd60c451f9d5d810bf522794b9bcaeb6f201
Gor <61106836+TheGor-365@users.noreply.github.com>
2023-03-04 12:24:23 +0300

    Add upvote and downvote buttons

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,19 @@
 class PagesController < ApplicationController
   def home
+    @post = Post.first if Post.any?
+  end
+
+  def upvote
+    @post = Post.find(params[:id])
+    @post.increment!(:upvote)
+    # @post.update(upvote: @post.upvote + 1)
+    redirect_to root_path
+  end
+
+  def downvote
+    @post = Post.find(params[:id])
+    @post.decrement!(:upvote)
+    # @post.update(upvote: @post.upvote + 1)
+    redirect_to root_path
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root "pages#home"
-
+  put 'posts/:id/upvote', to: 'pages#upvote', as: 'upvote_page'
+  put 'posts/:id/downvote', to: 'pages#downvote', as: 'downvote_page'
   resources :posts
 end
